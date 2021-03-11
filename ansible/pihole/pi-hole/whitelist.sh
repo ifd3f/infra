77f4a64b668c5ad3311cfc01316c15ce1e4563d2
Astrid Yu <astrid@astrid.tech>
2021-03-11 00:07:41 -0800

    Properly create Samba server

--- ansible/pihole/pi-hole/whitelist.sh
+++ ansible/pihole/pi-hole/whitelist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "::::: Adding whitelist items :::::"
-pihole --white-wild cloud
+pihole --white-wild lan
+pihole --white-wild cloud.astrid.tech
--- terraform/samba/main.tf
+++ terraform/samba/main.tf
@@ -20,19 +20,19 @@ provider "proxmox" {
   pm_tls_insecure = true
 }
 
-resource "random_password" "samba_root" {
+resource "random_password" "server_root" {
   length = 24
   special = false
 }
 
-resource "local_file" "samba_root" {
-  content = random_password.samba_root.result
+resource "local_file" "server_root" {
+  content = random_password.server_root.result
   filename = "${path.module}/credentials/server_root.txt"
 }
 
 resource "proxmox_lxc" "samba" {
   target_node = "thonkpad"
-  hostname = "samba"
+  hostname = "sambruh.lan"
   description = "ProxmoxResources server"
 
   vmid = 1000
@@ -42,21 +42,21 @@ resource "proxmox_lxc" "samba" {
   swap = 512
   cores = 2
 
+  unprivileged = false
   features {
-    fuse    = true
-    mount   = "nfs;cifs"
+    mount = "cifs"
+    nesting = true
   }
 
   ostemplate = "local:vztmpl/debian-10-turnkey-fileserver_16.0-1_amd64.tar.gz"
-  password = random_password.samba_root.result
-  unprivileged = false
+  password = random_password.server_root.result
   start = true
   onboot = true
   ssh_public_keys = var.ssh_pubkey
 
   rootfs {
-    storage = "local-lvm"
-    size = "300G"
+    storage = "thonkstor"
+    size = "100G"
   }
 
   network {
@@ -67,12 +67,12 @@ resource "proxmox_lxc" "samba" {
   }
 }
 
-resource "random_password" "samba_smb_root" {
+resource "random_password" "samba_root" {
   length = 24
   special = false
 }
 
-resource "local_file" "samba_smb_root" {
-  content = random_password.samba_smb_root.result
+resource "local_file" "samba_root" {
+  content = random_password.samba_root.result
   filename = "${path.module}/credentials/samba_root.txt"
 }
