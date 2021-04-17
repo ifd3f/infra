563d01b81da92ae114ed4400bbaf015567fa480e
Astrid Yu <astrid@astrid.tech>
2021-04-17 02:32:54 -0700

    Change janky method to be a shell script

--- kubernetes/matrix/configs/make-db-config.sh
+++ kubernetes/matrix/configs/make-db-config.sh
@@ -1,9 +1,13 @@
+#!/bin/sh
+
+cat <<-EOF
 database:
   name: psycopg2
   args:
-    user: {{ username }}
-    password: {{ password }}
+    user: $username
+    password: $password
     database: postgresql
     host: synapse-db
     cp_min: 5
     cp_max: 10
+EOF
