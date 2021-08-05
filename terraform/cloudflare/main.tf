0a965df21aa22993735f6d93ab4b7611caf73937
Astrid Yu <astrid@astrid.tech>
2021-08-05 02:20:04 +0000

    Move Cloudflare state into cloud

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "remote" {
+    organization = "astralbijection"
+
+    workspaces {
+      name = "cloudflare"
+    }
+  }
+
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
