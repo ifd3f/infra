5f0aa3f6ab3299ecbdba871e2cd92f2b59e62623
Astrid Yu <astrid@astrid.tech>
2021-08-14 19:21:58 +0000

    OpenWRT config downloads

--- openwrt/download-config.sh
+++ openwrt/download-config.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
-target=$1
-if [ -z "$target" ]; then 
-    echo "No openwrt host supplied"
-    exit 
-fi
+function download {
+    target=$1
+    shift
+    files="$@"
+    if [ -z "$target" ]; then 
+        >&2 echo "No openwrt host supplied"
+        exit 1
+    fi
 
-ssh $target uci export > config.uci
+
+    (
+        for a in $files; do 
+            echo uci export $a 
+        done
+    ) | ssh $target /bin/ash
+}
+
+# Export wireless separately because there's wifi passwords
+download >outer.uci root@192.168.1.1 \
+    dhcp \
+    dropbear \
+    firewall \
+    luci \
+    network \
+    rpcd \
+    system \
+    ubootenv \
+    ucitrack \
+    uhttpd 
+
+download >outer.secret.uci root@192.168.1.1 wireless
+
+download >travel.uci root@10.4.0.1 \
+    dhcp \
+    dropbear \
+    firewall \
+    luci \
+    network \
+    rpcd \
+    system \
+    ucitrack \
+    uhttpd 
+
+download >travel.secret.uci root@10.4.0.1 wireless
