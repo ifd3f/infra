b760eeda855c603b4d8d0c50e05b83c296f4ecf8
Astrid Yu <astrid@astrid.tech>
2021-08-15 17:55:19 -0700

    Move variables to host script

--- ansible/import_libvirt.sh
+++ ansible/import_libvirt.sh
@@ -12,4 +12,10 @@ ephemeral:
       ansible_ssh_pass: devpassword
       ansible_become_pass: devpassword
       ansible_python_interpreter: /usr/bin/python3
+      fqdn: bongus.hv.astrid.tech
+      short_hostname: bongus
+      interface: ens3
+      ip: 192.168.5.2
+      cidr: 24
+      gateway: 192.168.5.1
 EOF
