bf04a19c120da937ef5e0d0d6b6f0595f62b55bd
Astrid Yu <astrid@astrid.tech>
2021-08-21 01:05:18 -0700

    Move bongus partitioning script

--- scripts/partition/bongus.sh
+++ scripts/partition/bongus.sh
@@ -4,24 +4,24 @@
 
 # Ephemeral root idea "borrowed" from https://grahamc.com/blog/erase-your-darlings
 # We won't be using ZFS root, but XFS reformatted every boot
-export disk=/dev/disk/by-id/ata-QEMU_HARDDISK_QM00007
+export root_disk=/dev/disk/by-id/ata-QEMU_HARDDISK_QM00007
 
 # Make partition table and partitions
-sgdisk --zap-all $disk
-sgdisk -n1:0:+550M -t1:ef00 $disk
-sgdisk -n2:0:+10G -t2:bf00 $disk
-sgdisk -n3:0:0 -t2:bf00 $disk
+sgdisk --zap-all $root_disk
+sgdisk -n1:0:+550M -t1:ef00 $root_disk
+sgdisk -n2:0:+10G -t2:bf00 $root_disk
+sgdisk -n3:0:0 -t2:bf00 $root_disk
 
 sleep 2  # wait for /dev/disk/by-id to update
 
 # Format partitions
-export boot=$disk-part1
-export root=$disk-part2
-export pool=$disk-part3
+export boot=$root_disk-part1
+export root=$root_disk-part2
+export pool=$root_disk-part3
 
 mkfs.vfat $boot
 
-mkfs.xfs -f $root
+mkfs.ext4 $root
 
 zpool create rpool $pool
 zfs create -p -o mountpoint=legacy rpool/local/nix
@@ -29,7 +29,7 @@ zfs create -p -o mountpoint=legacy rpool/safe/persist
 
 # Create mountpoints and mount
 mkdir -p /mnt 
-mount -t xfs $root /mnt
+mount -t ext4 $root /mnt
 
 mkdir -p /mnt/nix /mnt/boot/efi /mnt/persist
 mount -t zfs rpool/local/nix /mnt/nix
