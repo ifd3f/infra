e67fd6517223d6bb8ca19afbdc6331315b34e8a3
Astrid Yu <astrid@astrid.tech>
2021-08-20 23:38:51 -0700

    I can't believe I made such a stupid typo

--- nixos/iso/iso.nix
+++ nixos/iso/iso.nix
@@ -13,18 +13,12 @@
     mutableUsers = false;
     users.nixos = {
       openssh.authorizedKeys.keys = [ 
-        # (import ../keys.nix).astrid 
-        "ssh-rsa AAAAB4NzaC1yc2EAAAADAQABAAABAQDc79RQW1memuqUUT7vYBKHBZ+h+BcjgCu0vvwgLLBwwJ3yBgU7wFx0EUkmfsg3icXhfiH61Bt7J4/eOm/sAE39oAcEZkv6xXgzwhMnWBWjAFcM5Ai3yvQJKxHVTkRXwyajf14HCQ594o0uLz0SGmoMvVkgBetdugRkvaYULdwgrqPt0302pEw1cKfUifeRqFnGVLcllXJV1iWqvuODfJTUO4tTlEIRSLcojaEfDVHM+9/Xx6tqFNeDxrRWd4VIf0vLbirCF8AzqzkYGjV9CL0Ao1l6serdLGZDtkpdd8gK5QQ1PNBqQPvMo+1p3Wq76Jy6dSax08GTdnG6/REUsWo3 astrid@BANANA"
+        (import ../keys.nix).astrid 
       ];
     };
   };
 
   environment.etc = {
-    # "install.sh" = {
-    #   source = ./install.sh;
-    #   mode = "0700";
-    # };
-
     "configuration.nix" = {
       source = ./configuration.nix;
       mode = "0600";
