5e17847ff66eec3495231c8bc654f84745a4169f
Astrid Yu <astrid@astrid.tech>
2021-09-06 14:42:59 -0700

    SSH key persistence

--- nixos/modules/sshd.nix
+++ nixos/modules/sshd.nix
@@ -1,10 +1,33 @@
 { ... }:
 {
+  systemd.services."ensure-sshd-dirs" = {
+    description = "Ensure directory for SSHD host keys exists";
+    script = ''
+      mkdir -p /persist/etc/ssh/
+    '';
+    wantedBy = [ "sshd.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+    };
+  };
+
   services = {
     openssh = {
       enable = true;
       passwordAuthentication = false;
       permitRootLogin = "yes";
+
+      hostKeys = [
+        {
+          path = "/persist/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+        {
+          path = "/persist/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+          bits = 4096;
+        }
+      ];
     };
   };
 
