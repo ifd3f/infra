ba02525707f2b4482e8cb2a79e7326605de9433d
Astrid Yu <astrid@astrid.tech>
2021-09-05 20:05:38 -0700

    Fix ACME breaking due to Cloudflare proxy

--- terraform/cloudflare/personal_site.tf
+++ terraform/cloudflare/personal_site.tf
@@ -2,43 +2,43 @@ resource "cloudflare_record" "personal_site_release" {
   for_each = toset(["staging", "beta"])
 
   zone_id = cloudflare_zone.primary.id
+  type    = "CNAME"
   name    = each.key
   value   = "cname.vercel-dns.com"
-  type    = "CNAME"
   proxied = false
 }
 
 resource "cloudflare_record" "personal_site_www" {
-  name    = "www"
-  proxied = false
-  type    = "CNAME"
-  value   = "astrid.tech"
   zone_id = cloudflare_zone.primary.id
+  type    = "CNAME"
+  name    = "www"
+  value   = "astrid.tech"
+  proxied = false
 }
 
 // Apex points to Vercel: https://vercel.com/docs/custom-domains#apex-domains
 resource "cloudflare_record" "personal_site_apex" {
   zone_id = cloudflare_zone.primary.id
+  type    = "A"
   name    = "@"
   value   = "76.76.21.21"
-  type    = "A"
   proxied = false
 }
 
 // API points to Oracle Cloud, which hosts the backend
 resource "cloudflare_record" "personal_site_api" {
-  name    = "api"
-  proxied = false
-  type    = "CNAME"
-  value   = "oci.h.astrid.tech"
   zone_id = cloudflare_zone.primary.id
+  type    = "CNAME"
+  name    = "api"
+  value   = "oci.h.astrid.tech"
+  proxied = false  // Do not proxy, or else ACME won't work
 }
 
 // Short apex points to Oracle Cloud, which hosts the backend
 resource "cloudflare_record" "personal_site_shortener" {
   zone_id = cloudflare_zone.short.id
+  type    = "A"
   name    = "@"
   value   = cloudflare_record.oci_public_ip.value
-  type    = "A"
-  proxied = true
+  proxied = false  // Do not proxy, or else ACME won't work
 }
