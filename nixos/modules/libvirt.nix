fb049243fc9581beafb125173e1118cd18090ee1
Astrid Yu <astrid@astrid.tech>
2021-09-06 17:33:07 -0700

    Persist libvirt state

--- nixos/modules/libvirt.nix
+++ nixos/modules/libvirt.nix
@@ -1,5 +1,23 @@
 { pkgs, ... }:
 {
+  systemd.services."ensure-libvirt-state" = {
+    description = "Ensure directory for libvirt state exists";
+    script = ''
+      mkdir -p /persist/var/lib/libvirt
+    '';
+    wantedBy = [ "var-lib-libvirt.mount" ];
+    serviceConfig = {
+      Type = "oneshot";
+    };
+  };
+
+  # Unfortunately, libvirt doesn't like symlinks to /var/lib/libvirt, but it's
+  # okay with bind mounts.
+  fileSystems."/var/lib/libvirt" = {
+    device = "/persist/var/lib/libvirt";
+    options = [ "bind" ];
+  };
+
   boot.kernelModules = [ "kvm-intel" ];
 
   virtualisation.libvirtd = {
