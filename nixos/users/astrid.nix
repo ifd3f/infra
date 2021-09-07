70a06b39f45cf95c8be25f5eb76b0eba8fc366eb
Astrid Yu <astrid@astrid.tech>
2021-09-07 16:40:17 -0700

    Customize my personal user a bit more

--- nixos/users/astrid.nix
+++ nixos/users/astrid.nix
@@ -1,8 +1,12 @@
 {
+  description = "Astrid Yu,astrid.tech/about,(805) 270-5368,nah,astrid@astrid.tech";
   openssh.authorizedKeys.keys = [ (import ../keys.nix).astrid ];
   isNormalUser = true;
   extraGroups = [ 
     "wheel"
     "libvirtd"
+    "dialout"
+    "docker"
+    "lxd"
   ];
 }
