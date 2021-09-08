10db8bd765c7e6e53a3187afc4c498d8604d2d80
Astrid Yu <astrid@astrid.tech>
2021-09-07 17:48:42 -0700

    Successfully boot a Talos VM!

--- terraform/system-k8s/main.tf
+++ terraform/system-k8s/main.tf
@@ -11,14 +11,25 @@ provider "libvirt" {
   uri = "qemu+ssh://astrid@192.168.1.112/system"
 }
 
+locals {
+  nix_ovmf_location = "/run/libvirt/nix-ovmf/OVMF_CODE.fd"
+}
+
 resource "libvirt_pool" "bongus_guests" {
   name = "guests"
   type = "dir"
   path = "/srv/guests"
 }
 
+resource "libvirt_network" "lan_bridge" {
+  name = "br0"
+  mode = "bridge"
+  bridge = "br0"
+}
+
+// Talos installation disk.
 resource "libvirt_volume" "talos_os" {
-  name   = "talos-amd64.iso"
+  name   = "talos-amd64-v0.12.1.iso"
   pool   = libvirt_pool.bongus_guests.name
   source = "https://github.com/talos-systems/talos/releases/download/v0.12.1/talos-amd64.iso"
 }
@@ -29,6 +40,33 @@ resource "libvirt_volume" "boot_disk" {
   size   = 20 * 1024 * 1024 * 1024 // 20GiB
 }
 
-# resource "libvirt_domain" "master" {
-#   name = "test"
-# }
+resource "libvirt_domain" "nixos_test" {
+  name        = "tiber-septim"
+  description = "Talos OS"
+  memory      = 4000
+  vcpu        = 2
+  autostart   = false
+  firmware    = local.nix_ovmf_location
+
+  network_interface {
+    network_name = libvirt_network.lan_bridge.name
+  }
+
+  disk {
+    volume_id = libvirt_volume.talos_os.id
+  }
+
+  disk {
+    volume_id = libvirt_volume.boot_disk.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+
+  xml {
+    xslt = file("modify.xslt")
+  }
+}
