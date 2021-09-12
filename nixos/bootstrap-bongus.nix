c50236101e37c896e00a2b8b198579228acad99b
Astrid Yu <astrid@astrid.tech>
2021-09-12 12:56:50 -0700

    Refactor bootstrap to use modules

--- nixos/bootstrap-bongus.nix
+++ nixos/bootstrap-bongus.nix
@@ -1,85 +1,29 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
+# A very simple module that installs from the flake on boot.
+# This is used because nixos-install --flake is broken.
 { config, pkgs, modulesPath, ... }:
-
 {
   imports =
     [
       (modulesPath + "/installer/scan/not-detected.nix")
+      ./modules/bm-server.nix
+      ./modules/debuggable.nix
+      ./modules/sshd.nix
+      ./modules/stable-flake.nix
+      ./modules/zfs-boot.nix
+      ./systems/bongus-hv/boot.nix
+      ./systems/bongus-hv/net.nix
+      (import ./systems/bongus-hv/fs.nix).module
     ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ata_piix" "uhci_hcd" "hpsa" "usb_storage" "sd_mod" ];
-  boot.kernelModules = [ "kvm-intel" ];
-
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-id/scsi-3600508b1001c5e757c79ba52c727a91f-part2";
-      fsType = "ext4";
-    };
-
-  fileSystems."/nix" =
-    {
-      device = "dpool/local/nix";
-      fsType = "zfs";
-    };
-
-  fileSystems."/persist" =
-    {
-      device = "dpool/safe/persist";
-      fsType = "zfs";
-    };
-
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-id/scsi-3600508b1001c5e757c79ba52c727a91f-part1";
-      fsType = "vfat";
-    };
-
-  nix = {
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
-  systemd.services."install-flake" = {
+  systemd.services."install-from-flake" = {
     script = ''
-      nixos-rebuild switch --flake github:astralbijection/infra#bongus-hv && reboot
+      nixos-rebuild boot --flake github:astralbijection/infra#bongus-hv && reboot
     '';
     wantedBy = [ "multi-user.target" ];
   };
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.copyKernels = true;
-  boot.loader.grub.device = "/dev/disk/by-id/scsi-3600508b1001c5e757c79ba52c727a91f"; # or "nodev" for efi only
-  boot.initrd.supportedFilesystems = [ "zfs" ]; # boot from zfs
-  boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.requestEncryptionCredentials = true;
-
-  networking.hostName = "bongus-hv"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   # Set your time zone.
   time.timeZone = "US/Pacific";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.hostId = "6d1020a1"; # Required for ZFS
-  networking.useDHCP = false;
-  networking.interfaces.eno1.useDHCP = true;
-  networking.interfaces.eno2.useDHCP = true;
-  networking.interfaces.eno3.useDHCP = true;
-  networking.interfaces.eno4.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -88,30 +32,6 @@
     keyMap = "us";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.astrid = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    openssh.authorizedKeys.keys = [ (import ./keys.nix).astrid ];
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    neovim
-    curl
-    git
-  ];
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  security.sudo.wheelNeedsPassword = false;
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  networking.firewall.enable = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -119,5 +39,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "unstable"; # Did you read the comment?
-
 }
