0b8879ec7c0866c682d885639bfc86414a0ad909
Astrid Yu <astrid@astrid.tech>
2021-09-12 13:23:19 -0700

    Use programs option instead of systemPackages for tmux, nvim

--- nixos/modules/debuggable.nix
+++ nixos/modules/debuggable.nix
@@ -1,17 +1,24 @@
 # A minimal environment, but it is still somewhat comfortable to debug.
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    # Editors are always helpful
-    neovim
+  programs = {
+    # Neovim is cool and good
+    neovim = {
+      enable = true;
+      viAlias = true;
+    };
 
+    # Just in case the SSH connection is lost and I'm running something long
+    tmux = {
+      enable = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
     # Download stuff from the internet
     git
     curl
     wget
-
-    # Just in case the SSH connection is lost and I'm running something long
-    tmux
 
     # Useful scripting utilities
     envsubst
