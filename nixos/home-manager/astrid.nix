68d0b546a5c1f2343f4c8c59a37b9eb1ce9aa5e0
Astrid Yu <astrid@astrid.tech>
2021-09-19 12:00:22 -0700

    Add VSCode server to home manager

--- nixos/home-manager/astrid.nix
+++ nixos/home-manager/astrid.nix
@@ -1,4 +1,16 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let vscodeTarball = fetchTarball {
+  url = "https://github.com/msteen/nixos-vscode-server/tarball/master";
+  sha256 = "sha256:14zqbjsm675ahhkdmpncsypxiyhc4c9kyhabpwf37q6qg73h8xz5";
+};
+in
+{
+  imports = [
+    "${vscodeTarball}/modules/vscode-server/home.nix"
+  ];
+
+  services.vscode-server.enable = true;
+
   programs.git = {
     enable = true;
     userName = "Astrid Yu";
