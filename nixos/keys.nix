d8156ef32115be8129264976578334bc5a2e34b3
Astrid Yu <astrid@astrid.tech>
2021-09-30 13:46:12 -0700

    nix: Add more SSH keys

--- nixos/keys.nix
+++ nixos/keys.nix
@@ -1,3 +1,15 @@
 {
-  astrid = builtins.readFile ../ssh_keys/astrid_banana_id_ed25519.pub;
+  astrid = [
+    builtins.readFile ../ssh_keys/astrid_banana_id_ed25519.pub
+    builtins.readFile ../ssh_keys/astrid_juicessh_id_ed25519.pub
+    builtins.readFile ../ssh_keys/astrid_cracktop-pc_id_ed25519.pub
+  ];
+
+  infra-repo = [
+    builtins.readFile ../ssh_keys/gh_rsa.pub
+  ];
+
+  cynthe = [
+    builtins.readFile ../ssh_keys/cynthe_id_rsa.pub
+  ];
 }
--- nixos/users/astrid.nix
+++ nixos/users/astrid.nix
@@ -1,6 +1,6 @@
 {
   description = "Astrid Yu,astrid.tech/about,(805) 270-5368,nah,astrid@astrid.tech";
-  openssh.authorizedKeys.keys = [ (import ../keys.nix).astrid ];
+  openssh.authorizedKeys.keys = (import ../keys.nix).astrid;
   isNormalUser = true;
   extraGroups = [ 
     "wheel"
