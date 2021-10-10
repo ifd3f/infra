a0dbf86d9936345305ce3b23f5a21f792625eace
Astrid Yu <astrid@astrid.tech>
2021-10-09 23:38:39 -0700

    nix: Disable SD image comperssion

--- nixos/systems/rpi-bootstrap-sd.nix
+++ nixos/systems/rpi-bootstrap-sd.nix
@@ -11,7 +11,11 @@ in nixpkgs.lib.nixosSystem {
       imports = [
         "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
       ];
+
+      # Don't compress the image.
+      sdImage.compressImage = false;
       
+      # Import Astrid's keys so it can be managed headlessly.
       users.extraUsers.root.openssh.authorizedKeys.keys = (import ../keys.nix).astrid;
     }
     (import ../modules/sshd.nix)
