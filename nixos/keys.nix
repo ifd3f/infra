754dd541c3269c909dc997160eee1275efc08742
Astrid Yu <astrid@astrid.tech>
2021-10-10 12:08:25 -0700

    nix: Add my key from jonathan-js

--- nixos/keys.nix
+++ nixos/keys.nix
@@ -3,6 +3,7 @@
     (builtins.readFile ../ssh_keys/astrid_banana_id_ed25519.pub)
     (builtins.readFile ../ssh_keys/astrid_juicessh_id_ed25519.pub)
     (builtins.readFile ../ssh_keys/astrid_cracktop-pc_id_ed25519.pub)
+    (builtins.readFile ../ssh_keys/astrid_jonathan-js_id_ed25519.pub)
   ];
 
   infra-repo = [ builtins.readFile ../ssh_keys/gh_rsa.pub ];
