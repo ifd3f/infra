e985cc7a02166ec22530c97dba4d4780d902f482
Astrid Yu <astrid@astrid.tech>
2021-10-12 00:37:39 -0700

    wg: Generate more keypairs

--- wireguard/default.nix
+++ wireguard/default.nix
@@ -1,3 +1,6 @@
 {
+  astrid-phone = builtins.readFile "./astrid-phone.pub";
   banana = builtins.readFile "./banana.pub";
+  cracktop = builtins.readFile "./cracktop.pub";
+  lighthouse = builtins.readFile "./lighthouse.pub";
 }
