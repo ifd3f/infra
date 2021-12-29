dbf11548084f26c4bd7205833b0143b8991d5089
Astrid Yu <astrid@astrid.tech>
2021-12-29 01:11:18 -0800

    nix: Reinstall gfdesk

--- nixos/systems/gfdesk/default.nix
+++ nixos/systems/gfdesk/default.nix
@@ -1,56 +1,11 @@
-# A chonky HP DL380P Gen8 rack server.
-
-{ self, nixpkgs-unstable, home-manager-unstable, ... }:
+# The desk that is used by Good Friends.
+inputs:
 let
-  nixpkgs = nixpkgs-unstable;
-  home-manager = home-manager-unstable;
-
-  fs = import ./fs.nix;
-  netModule = import ./net.nix;
-  bootModule = import ./boot.nix;
-
-  specialized = { config, lib, pkgs, ... }: {
-    time.timeZone = "US/Pacific";
-
-    ext4-ephroot.partition = fs.devices.rootPart;
-
-    # Explicitly don't reboot on kernel upgrade. This server takes forever to reboot, plus 
-    # it's a jet engine when it boots and it will probably wake me up at 4:00 AM
-    system.autoUpgrade.allowReboot = false;
-
-    # The Minecraft server port
-    networking.firewall.allowedTCPPorts = [ 25565 ];
-
-    users.users.alia = import ../../users/alia.nix;
-
-    home-manager = {
-      useGlobalPkgs = true;
-      useUserPackages = true;
-      # TODO refer to self.homeConfigurations."astrid@gfdesk" instead
-      users.astrid = self.homeModules.astrid_cli_full;
-    };
-  };
-
+  nixpkgs = inputs.nixpkgs-unstable;
+  home-manager = inputs.home-manager-unstable;
 in nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
 
-  modules = with self.nixosModules; [
-    bm-server
-    debuggable
-    ext4-ephroot
-    flake-update
-    home-manager.nixosModules.home-manager
-    libvirt
-    nix-dev
-    persistence
-    sshd
-    stable-flake
-    zfs-boot
-    zsh
-
-    bootModule
-    fs.module
-    netModule
-    specialized
-  ];
+  modules =
+    [ (import ./hardware-configuration.nix) (import ./specialized.nix inputs) ];
 }
---
+++ nixos/systems/gfdesk/specialized.nix
@@ -0,0 +1,69 @@
+{ self, home-manager-unstable, ... }:
+{ config, lib, pkgs, ... }: {
+  imports = with self.nixosModules; [
+    bm-server
+    debuggable
+    flake-update
+    home-manager-unstable.nixosModules.home-manager
+    libvirt
+    nix-dev
+    sshd
+    stable-flake
+    zfs-boot
+    zsh
+  ];
+
+  time.timeZone = "US/Pacific";
+
+  # Explicitly don't reboot on kernel upgrade. This server takes forever to reboot, plus 
+  # it's a jet engine when it boots and it will probably wake me up at 4:00 AM
+  system.autoUpgrade.allowReboot = false;
+
+  # a Very Good Friend's user
+  users.users.alia = import ../../users/alia.nix;
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+
+    # TODO refer to self.homeConfigurations."astrid@gfdesk" instead
+    users.astrid = self.homeModules.astrid_cli_full;
+  };
+
+  networking = {
+    hostName = "gfdesk";
+    domain = "id.astrid.tech";
+
+    hostId = "6d1020a1"; # Required for ZFS
+    useDHCP = false;
+
+    bridges."br0".interfaces = [ "eno1" ];
+    firewall.allowedTCPPorts = [ 25565 ];
+    interfaces = {
+      eno1.useDHCP = true;
+      eno2.useDHCP = true;
+      eno3.useDHCP = true;
+      eno4.useDHCP = true;
+      br0.useDHCP = true;
+    };
+  };
+
+  # Use the GRUB 2 boot loader.
+  boot = {
+    loader.grub = {
+      enable = true;
+      version = 2;
+      copyKernels = true;
+      # HP G8 only supports BIOS, not UEFI
+      device = "/dev/disk/by-id/wwn-0x600508b1001c5e757c79ba52c727a91f";
+    };
+
+    initrd = {
+      availableKernelModules =
+        [ "ehci_pci" "ata_piix" "uhci_hcd" "hpsa" "usb_storage" "sd_mod" ];
+      kernelModules = [ ];
+    };
+
+    extraModulePackages = [ ];
+  };
+}
