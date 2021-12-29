3944e1ca930a6021b7f6daa2827d2e55253e7bc1
Astrid Yu <astrid@astrid.tech>
2021-12-29 15:29:01 -0800

    nix: Fix banana not supporting multi-monitors

--- nixos/systems/banana/specialized.nix
+++ nixos/systems/banana/specialized.nix
@@ -21,6 +21,17 @@
   # so i can be a *gamer*
   programs.steam.enable = true;
 
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.prime = {
+    sync.enable = true;
+
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:1:0:0";
+
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+  };
+
   networking = {
     hostName = "banana";
     domain = "id.astrid.tech";
