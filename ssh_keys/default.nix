702cc71d932858f0622477bbc4b148821a2df36a
Astrid Yu <astrid@astrid.tech>
2022-01-14 11:13:58 -0800

    Update default.nix

--- ssh_keys/default.nix
+++ ssh_keys/default.nix
@@ -8,6 +8,7 @@
     (builtins.readFile ./astrid_jonathan-js_id_rsa.pub)
     (builtins.readFile ./astrid_joseph-js_id_rsa.pub)
     (builtins.readFile ./astrid_tablet-termux_id_ed25519.pub)
+    (builtins.readFile ./astrid_usb-keychain.pub)
   ];
 
   infra-repo = [ builtins.readFile ./gh_rsa.pub ];
