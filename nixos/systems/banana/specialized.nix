d19ed632e3a9de816b61321b001120c824009700
Astrid Yu <astrid@astrid.tech>
2022-01-24 09:12:27 -0800

    nix: Enable bluetooth on banana

--- nixos/systems/banana/specialized.nix
+++ nixos/systems/banana/specialized.nix
@@ -24,17 +24,25 @@
   programs.steam.enable = true;
 
   # Nvidia configs, following this page https://nixos.wiki/wiki/Nvidia
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.prime = {
-    # Offload mode for lower power usage https://nixos.wiki/wiki/Nvidia#offload_mode
-    offload.enable = true;
-    sync.enable = false;
+  services = {
+    xserver.videoDrivers = [ "nvidia" ];
+    blueman.enable = true;
+  };
 
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
+  hardware = {
+    nvidia.prime = {
+      # Offload mode for lower power usage https://nixos.wiki/wiki/Nvidia#offload_mode
+      offload.enable = true;
+      sync.enable = false;
 
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
+      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+      nvidiaBusId = "PCI:1:0:0";
+
+      # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+      intelBusId = "PCI:0:2:0";
+    };
+
+    bluetooth.enable = true;
   };
 
   specialisation = {
