c6ba926427e67d07837bcd013fdc2f4ed2fed76b
Astrid Yu <astrid@astrid.tech>
2022-02-20 12:38:48 -0800

    nix: Add shai-hulud SSH keys

--- ssh_keys/default.nix
+++ ssh_keys/default.nix
@@ -3,11 +3,12 @@
 
   astrid = [
     (builtins.readFile ./astrid_banana_id_ed25519.pub)
-    (builtins.readFile ./astrid_juicessh_id_ed25519.pub)
-    (builtins.readFile ./astrid_newphone_id_ed25519.pub)
     (builtins.readFile ./astrid_cracktop-pc_id_ed25519.pub)
     (builtins.readFile ./astrid_jonathan-js_id_rsa.pub)
     (builtins.readFile ./astrid_joseph-js_id_rsa.pub)
+    (builtins.readFile ./astrid_juicessh_id_ed25519.pub)
+    (builtins.readFile ./astrid_newphone_id_ed25519.pub)
+    (builtins.readFile ./astrid_shai-hulud_id_ed25519.pub)
     (builtins.readFile ./astrid_tablet-termux_id_ed25519.pub)
     (builtins.readFile ./astrid_usb-keychain.pub)
   ];
