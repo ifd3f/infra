b2f355b5da658f897a43519c618ff561dd0baa5c
Astrid Yu <astrid@astrid.tech>
2022-03-17 00:20:25 -0700

    Only get SSH keys from files ending in .pub

--- ssh_keys/default.nix
+++ ssh_keys/default.nix
@@ -1,14 +1,18 @@
-let
-  keysInDir = dirPath:
-    builtins.attrValues (
-      builtins.mapAttrs
-        (file: _: builtins.readFile "${dirPath}/${file}")
-        (builtins.readDir dirPath)
-    );
+with builtins; let
+  keysInDir = dirPath: concatLists (
+    attrValues (
+      mapAttrs (file: _:
+        if match ".*\\.pub" file == null
+          then []
+          else [ (replaceStrings ["\n"] [""] (readFile "${dirPath}/${file}")) ]
+      )
+      (readDir dirPath)
+    )
+  );
 
-  users = builtins.mapAttrs
+  users = mapAttrs
     (subdir: _: keysInDir ./users/${subdir})
-    (builtins.readDir ./users);
+    (readDir ./users);
 
 in {
   inherit users;
