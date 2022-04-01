7c0c80c2e5fb1888cdb07d01e91c2a6f79bcef27
Astrid Yu <astrid@astrid.tech>
2022-03-31 20:05:16 -0700

    nix: Remove dbeaver, add store optimization stuff

--- nixos/modules/nix-utils.nix
+++ nixos/modules/nix-utils.nix
@@ -1,7 +1,13 @@
 # Enables Nix Unstable and Flakes.
 { pkgs, ... }: {
   nix = {
+    # Auto-optimize/GC store
     autoOptimiseStore = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
 
     # Trusted users for remote config builds and uploads
     trustedUsers = [ "root" "@wheel" ];
