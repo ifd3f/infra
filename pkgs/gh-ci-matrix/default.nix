676c6f50da2fffbcb018d20fc8a8b3902ae3ba76
Astrid Yu <astrid@astrid.tech>
2022-04-03 18:50:50 -0700

    ci: Make nicer names for jobs

--- pkgs/gh-ci-matrix/default.nix
+++ pkgs/gh-ci-matrix/default.nix
@@ -2,21 +2,25 @@
 { lib, writeText, self }:
 let 
   x86_64-linux = lib.mapAttrsToList (k: v: {
+    name = "Nix check x86_64-linux.${k}";
     target = "checks.x86_64-linux.${k}";
     os = "ubuntu-latest";
   }) self.checks.x86_64-linux;
 
   aarch64-linux = lib.mapAttrsToList (k: v: {
+    name = "Nix check aarch64-linux.${k}";
     target = "checks.aarch64-linux.${k}";
     os = "ubuntu-latest";
   }) self.checks.aarch64-linux;
 
   x86_64-darwin = lib.mapAttrsToList (k: v: {
+    name = "Nix check x86_64-darwin.${k}";
     target = "checks.x86_64-darwin.${k}";
     os = "macos-latest";
   }) self.checks.x86_64-darwin;
 
   x86_64-linux-docker-images = map (k: {
+    name = "Nix dockertools ${k}";
     target = "packages.x86_64-linux.${k}";
     os = "ubuntu-latest";
   }) [
