24177edb5c9226740b253b9b027017d30d262676
Astrid Yu <astrid@astrid.tech>
2022-05-23 09:32:54 -0700

    hm: Improve xmonad shortcuts

--- home-manager/astral/gui/tabletwm/xmonad.hs
+++ home-manager/astral/gui/tabletwm/xmonad.hs
@@ -40,7 +40,6 @@ myConfig = ewmh $ def
 main = do
     xmonad $ docks myConfig 
 
-
 xdisplays :: X [Rectangle]
 xdisplays = withDisplay $ io . getScreenInfo
 
@@ -85,46 +84,43 @@ myKeybinds conf@(XConfig {XMonad.terminal = terminal, XMonad.modMask = modMask})
         bindsList :: [((ButtonMask, KeySym), (String, X ()))]
         bindsList =
             [ ((modMask,                 xK_Return), ("Launch terminal " ++ terminal, spawn terminal))
-            , ((modMask .|. shiftMask,   xK_c     ), ("Close the focused window", kill))
+            , ((modMask .|. shiftMask,   xK_q     ), ("Close the focused window", kill))
 
             , ((modMask,                 xK_space ), ("Rotate through the available layout algorithms", sendMessage NextLayout))
-            , ((modMask .|. shiftMask,   xK_space ), ("Reset the layouts on the current workspace to default", setLayout $ XMonad.layoutHook conf))
-            , ((modMask,                 xK_d     ),  ("Spawn the default launcher", spawnLauncher))
+            , ((modMask .|. shiftMask,   xK_space ), ("Reset layouts on the current workspace to default", setLayout $ XMonad.layoutHook conf))
+            , ((modMask,                 xK_d     ), ("Spawn launcher", spawnLauncher))
 
             , ((modMask,                 xK_n     ), ("Resize viewed windows to the correct size", refresh))
 
             -- move focus up or down the window stack
-            , ((modMask,                 xK_Tab   ), ("Move focus to the next window", windows W.focusDown))
-            , ((modMask .|. shiftMask,   xK_Tab   ), ("Move focus to the previous window", windows W.focusUp  ))
-            , ((modMask,                 xK_j     ), ("Move focus to the next window", windows W.focusDown))
-            , ((modMask,                 xK_k     ), ("Move focus to the previous window", windows W.focusUp  ))
-            , ((modMask,                 xK_m     ), ("Move focus to the master window", windows W.focusMaster  ))
+            , ((modMask,                 xK_Tab   ), ("Focus next window", windows W.focusDown))
+            , ((modMask .|. shiftMask,   xK_Tab   ), ("Focus previous window", windows W.focusUp))
+            , ((modMask,                 xK_j     ), ("Focus next window", windows W.focusDown))
+            , ((modMask,                 xK_k     ), ("Focus previous window", windows W.focusUp))
+            , ((modMask,                 xK_m     ), ("Focus master window", windows W.focusMaster))
 
             -- modifying the window order
-            , ((modMask .|. shiftMask,   xK_Return), ("Swap the focused window and the master window", windows W.swapMaster))
-            , ((modMask .|. shiftMask,   xK_j     ), ("Swap the focused window with the next window", windows W.swapDown  ))
-            , ((modMask .|. shiftMask,   xK_k     ), ("Swap the focused window with the previous window", windows W.swapUp    ))
+            , ((modMask .|. shiftMask,   xK_Return), ("Swap focused window and master window", windows W.swapMaster))
+            , ((modMask .|. shiftMask,   xK_j     ), ("Swap focused window with next window", windows W.swapDown))
+            , ((modMask .|. shiftMask,   xK_k     ), ("Swap focused window with previous window", windows W.swapUp))
 
             -- resizing the master/slave ratio
-            , ((modMask,                 xK_h     ), ("Shrink the master area", sendMessage Shrink))
-            , ((modMask,                 xK_l     ), ("Expand the master area", sendMessage Expand))
+            , ((modMask,                 xK_h     ), ("Shrink master area", sendMessage Shrink))
+            , ((modMask,                 xK_l     ), ("Expand master area", sendMessage Expand))
 
             -- floating layer support
-            , ((modMask,                 xK_t     ), ("Push window back into tiling", withFocused $ windows . W.sink))
+            , ((modMask .|. shiftMask,   xK_f     ), ("Float window", withFocused toggleFloat))
 
             -- increase or decrease number of windows in the master area
-            , ((modMask .|. controlMask, xK_h     ), ("Increment the number of windows in the master area", sendMessage (IncMasterN 1)))
-            , ((modMask .|. controlMask, xK_l     ), ("Deincrement the number of windows in the master area", sendMessage (IncMasterN (-1))))
+            , ((modMask .|. controlMask, xK_h     ), ("More windows in master area", sendMessage (IncMasterN 1)))
+            , ((modMask .|. controlMask, xK_l     ), ("Less windows in master area", sendMessage (IncMasterN (-1))))
 
-            -- quit, or restart
-            , ((modMask .|. shiftMask,   xK_q     ), ("Quit XMonad", io (exitWith ExitSuccess)))
-
+            -- a help prompt for convenience
             , ((modMask,                 xK_slash ), ("Display help prompt", spawnHelp))
-            -- repeat the binding for non-American layout keyboards
 
             -- restart/quit
-            , ((modMask .|. shiftMask,   xK_r     ), ("Restart XMonad", restartXMonad))  -- note that we do not compile, home-manager does it for us
-            , ((modMask .|. shiftMask,   xK_e     ), ("Quit XMonad with a prompt.", confirmPrompt amberXPConfig "exit" $ io exitSuccess))
+            , ((modMask .|. controlMask, xK_r     ), ("Restart XMonad", restartXMonad))  -- note that we do not compile, home-manager does it for us
+            , ((modMask .|. controlMask, xK_e     ), ("Quit XMonad with a prompt", confirmPrompt amberXPConfig "exit" $ io exitSuccess))
             ]
             ++
             -- mod-[1..9] %! Switch to workspace N, and 0 goes to 10
@@ -138,6 +134,10 @@ myKeybinds conf@(XConfig {XMonad.terminal = terminal, XMonad.modMask = modMask})
             [((mod .|. modMask, key), (hf sc, screenWorkspace sc >>= flip whenJust (windows . wf)))
                 | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
                 , (wf, mod, hf) <- [(W.view, 0, ("Switch to screen " ++) . show), (W.shift, shiftMask, ("Move to screen " ++) . show)]]
+
+toggleFloat w = windows (\s -> if M.member w (W.floating s)
+    then W.sink w s
+    else (W.float w (W.RationalRect (1/3) (1/4) (1/2) (4/5)) s))
 
 buttonMaskToPrefix :: ButtonMask -> String
 buttonMaskToPrefix bm = addKey mod4Mask "M" ++ addKey controlMask "C" ++ addKey shiftMask "S" ++ addKey mod1Mask "A"
