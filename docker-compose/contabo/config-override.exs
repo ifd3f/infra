73937d04dbd8cec28674fc6b4e57dada7cd39462
Astrid Yu <astrid@astrid.tech>
2022-08-02 10:38:59 -0700

    akkoma: Move config items around

--- docker-compose/contabo/config-override.exs
+++ docker-compose/contabo/config-override.exs
@@ -3,13 +3,21 @@ import config
 config :pleroma, :instance,
   registrations_open: false
 
+config :pleroma, :media_proxy,
+  enabled: false,
+  redirect_on_failure: true,
+  base_url: "https://cache.domain.tld"
+
 # S3 upload setup
+config :pleroma, Pleroma.Upload,
+  uploader: Pleroma.Uploaders.S3,
+  strip_exif: true
+
 config :ex_aws, :s3,
-  access_key_id: System.get_env("S3_ACCESS_KEY_ID"),
-  secret_access_key: System.get_env("S3_ACCESS_KEY_SECRET"),
-  host: System.get_env("S3_HOST")
+  access_key_id: System.get_env("B2_APP_KEY_ID"),
+  secret_access_key: System.get_env("B2_APP_KEY"),
+  host: "s3.us-west-000.backblazeb2.com"
 
 config :pleroma, Pleroma.Uploaders.S3,
-  bucket: System.get_env("S3_BUCKET"),
-  public_endpoint: System.get_env("S3_PUBLIC_ENDPOINT")
-
+  bucket: System.get_env("B2_BUCKET"),
+  public_endpoint: "https://s3.us-west-000.backblazeb2.com"
