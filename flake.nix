16214edace7d71447dac9fad8a2891ae9c3f4466
Astrid Yu <astrid@astrid.tech>
2022-09-17 03:46:56 -0700

    hm: Refactor out long declarations into own file

--- flake.nix
+++ flake.nix
@@ -75,50 +75,7 @@
         };
 
         homeModule = self.homeModules.astral;
-        homeModules = {
-          astral = import ./home-manager/astral;
-
-          astral-cli = {
-            imports = [ self.homeModules.astral ];
-            astral.vi.enable = true;
-          };
-
-          astral-cli-full = {
-            imports = [ self.homeModules.astral-cli ];
-            astral.cli.extended = true;
-            astral.vi.ide = true;
-          };
-
-          astral-macos = {
-            imports = [ self.homeModules.astral ];
-
-            astral.cli = {
-              # enable = true;
-              extended = true;
-            };
-            astral.vi = {
-              enable = true;
-              ide = true;
-            };
-            astral.macos.enable = true;
-          };
-
-          astral-scientific = {
-            imports = [ self.homeModules.astral-cli ];
-            astral.cli.conda-hooks.enable = true;
-          };
-
-          astral-gui = {
-            imports = [ self.homeModules.astral-cli-full ];
-            astral.gui.enable = true;
-            astral.gui.xmonad.enable = true;
-          };
-
-          astral-gui-tablet = {
-            imports = [ self.homeModules.astral-gui ];
-            astral.gui.xmonad.enable = true;
-          };
-        };
+        homeModules = import ./home-manager/astral/variants.nix;
 
         homeConfiguration = self.homeModules.astral-cli;
         homeConfigurations = {
