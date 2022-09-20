4a403974bfb81281fa7aa2fa1797580ef948e170
Astrid Yu <astrid@astrid.tech>
2022-09-20 14:31:53 -0700

    where we're going we really don't need devcontainers at all

--- .devcontainer/bin/graphviz-online.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# A script that reads Graphviz code from stdin and
-# produces a URL for visualizing it.
-
-graphviz=$(cat - )
-
-jq -rn --arg x "$graphviz" \
-    '"https://dreampuf.github.io/GraphvizOnline/#" + ($x|@uri)'
