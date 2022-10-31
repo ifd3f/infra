34f47945eedf1c66bc7cca6053e8384dec6146a9
Astrid Yu <astrid@astrid.tech>
2022-10-30 17:30:03 -0700

    tf/cf: add diluc to dns

--- terraform/cloudflare/hosts.tf
+++ terraform/cloudflare/hosts.tf
@@ -5,3 +5,11 @@ resource "cloudflare_record" "oci_public_ip" {
   value   = "152.67.253.45"
   zone_id = cloudflare_zone.primary.id
 }
+
+resource "cloudflare_record" "diluc_public_ip" {
+  name    = "diluc.id"
+  proxied = false
+  type    = "A"
+  value   = "173.212.242.107"
+  zone_id = cloudflare_zone.primary.id
+}
