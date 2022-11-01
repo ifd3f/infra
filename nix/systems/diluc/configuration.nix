d99f652d48a294be82c000346bbd0ebc8e157c39
Astrid Yu <astrid@astrid.tech>
2022-11-01 13:17:28 -0700

    nixos/diluc: Refactor akkoma into its own file

--- nix/systems/diluc/configuration.nix
+++ nix/systems/diluc/configuration.nix
@@ -1,6 +1,6 @@
 # Contabo VPS.
 { pkgs, lib, ... }: {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [ ./hardware-configuration.nix ./akkoma.nix ];
 
   astral.roles.server.enable = true;
 
@@ -19,60 +19,24 @@
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
-  services.akkoma = {
-    enable = true;
-    config = let inherit ((pkgs.formats.elixirConf { }).lib) mkRaw;
-    in {
-      ":pleroma"."Pleroma.Web.Endpoint".url.host = "fedi.astrid.tech";
-      ":pleroma".":media_proxy".enabled = true;
-      ":pleroma".":instance" = {
-        name = "da astrid z0ne";
-        description = "astrid's akkoma server";
-        email = "akkoma@astrid.tech";
-        notify_email = "akkoma@astrid.tech";
-        registrations_open = false;
-      };
-
-      ":pleroma"."Pleroma.Upload" = {
-        uploader = mkRaw "Pleroma.Uploaders.S3";
-        base_url = "https://s3.us-west-000.backblazeb2.com";
-        strip_exif = false;
-      };
-      ":pleroma"."Pleroma.Uploaders.S3".bucket = "nyaabucket";
-      ":ex_aws".":s3" = {
-        access_key_id._secret = "/var/lib/secrets/akkoma/b2_app_key_id";
-        secret_access_key._secret = "/var/lib/secrets/akkoma/b2_app_key";
-        host = "s3.us-west-000.backblazeb2.com";
-      };
-    };
-
-    nginx = {
-      enableACME = true;
-      forceSSL = true;
-    };
-  };
-
   services.nginx.enable = true;
 
-  services.postgresql.enable = true;
-
   virtualisation.vmVariant = {
-    services.akkoma.nginx = {
-      enableACME = lib.mkForce false;
-      forceSSL = lib.mkForce false;
-    };
-
     virtualisation.forwardPorts = [
       {
         from = "host";
         host.port = 2222;
         guest.port = 22;
       }
-
       {
         from = "host";
         guest.port = 80;
         host.port = 8080;
+      }
+      {
+        from = "host";
+        guest.port = 443;
+        host.port = 8443;
       }
     ];
   };
