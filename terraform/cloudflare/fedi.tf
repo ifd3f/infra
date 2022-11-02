db9e35c2f7febd8067bdd29492dfbc5df863ec6a
Astrid Yu <astrid@astrid.tech>
2022-11-02 11:05:27 -0700

    tf/ext: disable secret uploading

--- terraform/cloudflare/fedi.tf
+++ terraform/cloudflare/fedi.tf
@@ -5,23 +5,23 @@ resource "b2_application_key" "main_fedi" {
   capabilities = ["deleteFiles", "listBuckets", "listFiles", "readBucketEncryption", "readBucketReplications", "readBuckets", "readFiles", "shareFiles", "writeBucketEncryption", "writeBucketReplications", "writeFiles"]
 }
 
-resource "remote_file" "main_fedi_b2_app_key" {
-  provider = remote.diluc
-
-  path        = "/var/lib/secrets/akkoma/b2_app_key"
-  content     = b2_application_key.main_fedi.application_key
-  owner       = "akkoma"
-  permissions = "0600"
-}
-
-resource "remote_file" "main_fedi_b2_app_key_id" {
-  provider = remote.diluc
-
-  path        = "/var/lib/secrets/akkoma/b2_app_key_id"
-  content     = b2_application_key.main_fedi.application_key_id
-  owner       = "akkoma"
-  permissions = "0600"
-}
+// resource "remote_file" "main_fedi_b2_app_key" {
+//   provider = remote.diluc
+// 
+//   path        = "/var/lib/secrets/akkoma/b2_app_key"
+//   content     = b2_application_key.main_fedi.application_key
+//   owner       = "akkoma"
+//   permissions = "0600"
+// }
+// 
+// resource "remote_file" "main_fedi_b2_app_key_id" {
+//   provider = remote.diluc
+// 
+//   path        = "/var/lib/secrets/akkoma/b2_app_key_id"
+//   content     = b2_application_key.main_fedi.application_key_id
+//   owner       = "akkoma"
+//   permissions = "0600"
+// }
 
 resource "cloudflare_record" "main_fedi" {
   name    = "fedi"
