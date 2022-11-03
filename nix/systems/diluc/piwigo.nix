bd3c814cf593c5d4f5a6d2f1e580fee28c9ecce3
Astrid Yu <astrid@astrid.tech>
2022-11-03 12:38:14 -0700

    Service for copying piwigo source

--- nix/systems/diluc/piwigo.nix
+++ nix/systems/diluc/piwigo.nix
@@ -6,13 +6,31 @@ let
     rev = "13.1.0";
     hash = "sha256-T3R5zfuDl1y3iNUrbouZlljDaNdtBTNro46fr+V67Oo=";
   };
+
+  webroot = "/var/www/photos";
 in {
+  systemd.services.piwigo-config = {
+    description = "Copy Piwigo source code to directory";
+    after = [ "network-online.target" ];
+    wantedBy = [ "phpfpm-piwigo-pool.service" ];
+    script = ''
+      mkdir -p ${webroot}
+      cp -r ${piwigo}/* ${webroot}
+      chmod 755 ${webroot}
+
+      mkdir -p ${webroot}/_data
+      chmod 777 ${webroot}/_data
+
+      chown -R piwigo:piwigo ${webroot}
+    '';
+  };
+
   services.nginx = {
     enable = true;
     virtualHosts."photos.astrid.tech" = {
       enableACME = true;
       forceSSL = true;
-      root = piwigo;
+      root = webroot;
 
       extraConfig = ''
         index index.php index.htm index.html;
@@ -29,11 +47,11 @@ in {
     enable = true;
     package = pkgs.mariadb;
 
-    ensureDatabases = [ "photos" ];
+    ensureDatabases = [ "piwigo" ];
 
     ensureUsers = [
       {
-        name = "photos";
+        name = "piwigo";
         ensurePermissions = { "piwigo.*" = "ALL PRIVILEGES"; };
       }
       {
@@ -43,8 +61,14 @@ in {
     ];
   };
 
+  users.groups.piwigo = { };
+  users.users."piwigo" = {
+    group = "piwigo";
+    isSystemUser = true;
+  };
+
   services.phpfpm.pools.piwigo-pool = {
-    user = "nobody";
+    user = "piwigo";
     settings = {
       pm = "dynamic";
       "listen.owner" = config.services.nginx.user;
