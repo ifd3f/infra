53d8e1d1f61f5340f9ec013eaa726f17ad904d01
Astrid Yu <astrid@astrid.tech>
2022-11-05 17:58:31 -0700

    Import zone files in module

--- nix/nixos-modules/astral/roles/auth-dns/default.nix
+++ nix/nixos-modules/astral/roles/auth-dns/default.nix
@@ -1,11 +1,37 @@
-{ pkgs, lib, ... }: {
-  services.bind = {
+{ pkgs, lib, config, ... }:
+with lib; {
+  options.astral.roles.auth-dns.enable =
+    mkEnableOption "astral authoritative DNS";
+
+  config.services.bind = let cfg = config.astral.roles.auth-dns;
+  in mkIf cfg.enable {
     enable = true;
-    extraOptions = "empty-zones-enable no;";
-    zones = [{
-      name = "astrid.tech";
-      master = true;
-      file = "";
-    }];
+
+    extraOptions = ''
+      empty-zones-enable no;
+    '';
+
+    zones = [
+      {
+        name = "astrid.tech";
+        master = true;
+        file = ./astrid.tech.zone;
+      }
+      {
+        name = "aay.tw";
+        master = true;
+        file = ./aay.tw.zone;
+      }
+      {
+        name = "0q4.org";
+        master = true;
+        file = ./0q4.org.zone;
+      }
+      {
+        name = "astridyu.com";
+        master = true;
+        file = ./astridyu.com.zone;
+      }
+    ];
   };
 }
