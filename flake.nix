f09da0fb170ef86a6820995fa1b98f14002779b9
Astrid Yu <astrid@astrid.tech>
2022-11-07 13:07:10 -0800

    cleanup flake a bit

--- flake.nix
+++ flake.nix
@@ -114,11 +114,13 @@
           };
         };
 
-        homeModule = self.homeModules.astral;
-        homeModules = import ./nix/home-manager/astral/variants.nix;
+        homeModules = (import ./nix/home-manager/astral/variants.nix) // {
+          default = self.homeModules.astral;
+        };
 
-        homeConfiguration = self.homeModules.astral-cli;
         homeConfigurations = {
+          default = self.homeModules.astral-cli;
+
           "astrid@aliaconda" = home-manager.lib.homeManagerConfiguration {
             pkgs = nixpkgs.legacyPackages.x86_64-linux;
             modules = [ self.homeModules.astral-scientific vscode-server-home ];
@@ -127,7 +129,6 @@
             pkgs = nixpkgs.legacyPackages.x86_64-linux;
             modules = [
               self.homeModules.astral-gui
-              vscode-server-home
               { xresources.properties = { "*.dpi" = 96; }; }
             ];
           };
@@ -135,8 +136,7 @@
             pkgs = nixpkgs.legacyPackages.x86_64-linux;
             modules = [
               self.homeModules.astral-gui
-              vscode-server-home
-              { xresources.properties = { "*.dpi" = 163; }; }
+              { xresources.properties = { "*.dpi" = 200; }; }
             ];
           };
           "astrid@Discovery" = home-manager.lib.homeManagerConfiguration {
@@ -149,35 +149,7 @@
           };
           "astrid@shai-hulud" = home-manager.lib.homeManagerConfiguration {
             pkgs = nixpkgs.legacyPackages.x86_64-linux;
-            modules = [
-              self.homeModules.astral-gui-tablet
-              vscode-server-home
-              (let dpi = 192;
-              in {
-                imports = [ self.homeModules.astral-gui-tablet ];
-                xresources.properties = { "*.dpi" = dpi; };
-                programs.rofi.extraConfig.dpi = dpi;
-                programs.autorandr = {
-                  enable = true;
-                  profiles.portable = {
-                    fingerprint = {
-                      eDP-1 =
-                        "00ffffffffffff0030e45505a1000010001a0104a51a117803ee95a3544c99260f5054000000010101010101010101010101010101013f7fb0a0a020347030203a0004ad10000019000000fd00303c000021040a141414141414000000fe004c47445f4d50302e325f0a2020000000fe004c503132335751313132363034003f";
-                    };
-                    config.eDP-1 = {
-                      enable = true;
-                      primary = true;
-                      mode = "2736x1824";
-                      rate = "60.00";
-                      position = "0x0";
-                      crtc = 0;
-                      # 267 PPI (https://www.microsoft.com/en-us/surface/devices/surface-pro-6)
-                      dpi = dpi;
-                    };
-                  };
-                };
-              })
-            ];
+            modules = [ self.homeModules.astral-gui-tablet ];
           };
           "astrid@soulcaster" = home-manager.lib.homeManagerConfiguration {
             pkgs = nixpkgs.legacyPackages.x86_64-linux;
