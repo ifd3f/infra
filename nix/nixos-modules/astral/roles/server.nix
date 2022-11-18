237e7ff88ad4e358b48bf8577a062e5f80161ea8
Astrid Yu <astrid@astrid.tech>
2022-11-17 16:06:04 -0800

    Fix missing declaration

--- nix/nixos-modules/astral/roles/server.nix
+++ nix/nixos-modules/astral/roles/server.nix
@@ -21,6 +21,7 @@
       custom-tty.enable = true;
       infra-update.enable = false;
       net.sshd.enable = true;
+      roles.monitoring.node.enable = true;
 
       users = {
         github.enable = true;
