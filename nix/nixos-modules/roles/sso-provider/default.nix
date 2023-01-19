0e1bb7fa5a324349f7139a9bc2165e11ddcdabd7
Astrid Yu <astrid@astrid.tech>
2023-01-19 11:25:07 -0800

    Revert "sso: Create Authelia service"
    This reverts commit 9e884c37c56d19bc43288bf3ee98567b6a945d82.

--- nix/nixos-modules/roles/sso-provider/default.nix
+++ nix/nixos-modules/roles/sso-provider/default.nix
@@ -1,96 +1,71 @@
 { pkgs, lib, config, ... }:
+with lib;
 let
-  cfg = {
-    theme = "auto";
-    default_redirection_url = "https://astrid.tech/";
+  kcfg = config.services.keycloak;
 
-    server = {
-      disable_healthcheck = false;
-      enable_expvars = false;
-      enable_pprof = false;
-      host = "0.0.0.0";
-      path = "";
-      port = 9091;
-    };
-
-    authentication_backend = {
-      refresh_interval = "1m";
-      ldap = {
-        implementation = "activedirectory";
-        url = "ldaps://ipa0.id.astrid.tech";
-        timeout = "5s";
-        tls.server_name = "ipa0.id.astrid.tech";
-        base_dn = "dc=astrid,dc=tech";
-        username_attribute = "uid";
-      };
-    };
-    log.level = "debug";
-    ntp = {
-      address = "time.cloudflare.com:123";
-      disable_failure = false;
-      disable_startup_check = false;
-      max_desync = "3s";
-      version = 4;
-    };
-    password_policy = {
-      standard = {
-        enabled = false;
-        max_length = 0;
-        min_length = 8;
-        require_lowercase = true;
-        require_number = true;
-        require_special = true;
-        require_uppercase = true;
-      };
-      zxcvbn = {
-        enabled = false;
-        min_score = 3;
-      };
-    };
-    regulation = {
-      ban_time = "5m";
-      find_time = "2m";
-      max_retries = 3;
-    };
-    session = {
-      expiration = "1h";
-      inactivity = "5m";
-      name = "authelia_session";
-      remember_me = "1M";
-      same_site = "lax";
-    };
-    telemetry.metrics = {
-      enabled = true;
-      address = "tcp://0.0.0.0:9959";
-    };
-  };
-
-  validatedCfg = with pkgs;
-    runCommand "validated" { json = builtins.toJSON cfg; } ''
-      echo "$json" > $out
-      ${authelia-bin}/bin/authelia validate-config --config $out
+  keystore = rec {
+    password = "thepasswordidkitdoesntmatterfuckthisshit";
+    file = "${deriv}";
+    deriv = pkgs.runCommand "keystore.jks" { } ''
+      ${pkgs.jdk}/bin/keytool \
+        -storepass ${password} \
+        -noprompt \
+        -trustcacerts \
+        -import \
+        -alias ipa0.id.astrid.tech \
+        -keystore $out \
+        -file ${./ipa0-id-astrid-tech.crt}
     '';
-in {
-  systemd.services.authelia = {
-    description = "Authelia authentication and authorization server";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "multi-user.target" ];
-
-    path = with pkgs; [ authelia-bin ];
-    script = "authelia --config ${validatedCfg}";
-
-    serviceConfig.SyslogIdentifier = "authelia";
   };
+in {
+  imports = [ ./ldap-shim.nix ];
 
   # Trust the LDAP server's cert
   security.pki.certificateFiles = [ ./ipa0-id-astrid-tech.crt ];
 
-  services.nginx.virtualHosts."sso.astrid.tech" = {
+  services.keycloak = {
+    enable = true;
+    settings = {
+      proxy = "edge";
+      hostname = "sso.astrid.tech";
+      http-port = 18433;
+      http-host = "0.0.0.0";
+      http-enabled = true;
+
+      spi-truststore-file-file = keystore.file;
+      spi-truststore-file-password = keystore.password;
+
+      log-level = "DEBUG";
+    };
+    database = {
+      type = "postgresql";
+      name = "keycloak";
+      username = "username";
+      host = "localhost";
+      passwordFile = "/var/lib/secrets/keycloak/dbpassword";
+    };
+  };
+
+  # # Make Java trust it too
+  # systemd.services.keycloak.environment.JAVA_ARGS =
+  #   "-Djavax.net.ssl.trustStore=${keystore} -Djavax.net.ssl.trustStorePassword=${password}";
+
+  services.postgresql = {
+    ensureDatabases = [ kcfg.database.name ];
+    ensureUsers = [{
+      name = kcfg.database.username;
+      ensurePermissions = {
+        "DATABASE \"${kcfg.database.name}\"" = "ALL PRIVILEGES";
+      };
+    }];
+  };
+
+  services.nginx.virtualHosts.${kcfg.settings.hostname} = {
     enableACME = true;
     forceSSL = true;
 
     locations."/" = {
-      proxyPass = "http://127.0.0.1:${builtins.toString cfg.server.port}";
+      proxyPass = "http://127.0.0.1:${toString kcfg.settings.http-port}";
       proxyWebsockets = true;
       extraConfig = ''
         proxy_set_header X-Forwarded-Proto $scheme;
