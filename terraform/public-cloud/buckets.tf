c1c8127dc2097efba1a05569e8186b9e00ee295c
Astrid Yu <astrid@astrid.tech>
2023-01-19 23:25:08 -0800

    Make a bucket for backups

--- terraform/public-cloud/buckets.tf
+++ terraform/public-cloud/buckets.tf
@@ -7,3 +7,8 @@ resource "b2_bucket" "logging" {
   bucket_name = "ifd3f-logging"
   bucket_type = "allPublic"
 }
+
+resource "b2_bucket" "backup" {
+  bucket_name = "ifd3f-backup"
+  bucket_type = "allPrivate"
+}
