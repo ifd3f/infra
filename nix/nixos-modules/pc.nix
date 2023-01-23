61724253e5bc4acc715220493d44641f631e2f0e
Astrid Yu <astrid@astrid.tech>
2023-01-22 21:55:15 -0800

    Fix PC break

--- nix/nixos-modules/pc.nix
+++ nix/nixos-modules/pc.nix
@@ -91,10 +91,7 @@
         virt-manager.enable = true;
       };
     };
-    net = {
-      xrdp.enable = true;
-      sshd.enable = true;
-    };
+    net.sshd.enable = true;
     zfs-utils.enable = true;
     # infra-update = {
     #   enable = true;
