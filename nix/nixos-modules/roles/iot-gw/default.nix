a381046fa4fb6b0a28d2d639f6e24acc7d6fba3c
Astrid Yu <astrid@astrid.tech>
2023-01-25 22:33:46 -0800

    iot-gw: Connect z2m to local mosquitto

--- nix/nixos-modules/roles/iot-gw/default.nix
+++ nix/nixos-modules/roles/iot-gw/default.nix
@@ -1,10 +1,17 @@
 # IoT Gateway, running at home.
 { config, pkgs, lib, ... }:
 let
+  vs = config.vault-secrets.secrets.iot-gw-s02;
+
   zigbeeDongle =
     "/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_6aa3627f3e98ec119bbbaad044d80d13-if00-port0";
 
 in with lib; {
+  # vault kv put kv/iot-gw-s02/environment \
+  #   ZIGBEE2MQTT_CONFIG_MQTT_USER=@ \
+  #   ZIGBEE2MQTT_CONFIG_MQTT_PASSWORD=@
+  vault-secrets.secrets.iot-gw-s02 = { };
+
   virtualisation.oci-containers = {
     backend = "podman";
     containers.homeassistant = {
@@ -19,7 +26,7 @@ in with lib; {
     enable = true;
     settings = {
       serial.port = zigbeeDongle;
-      network_key = "!/var/lib/secrets/z2m.yml network_key";
+      permit_join = true;
       frontend = {
         host = "0.0.0.0";
         port = 38323;
@@ -29,10 +36,9 @@ in with lib; {
       mqtt = {
         base_topic = "zigbee2mqtt-s02";
         client_id = "ghoti-zigbee2mqtt";
+        user = "zigbee2mqtt";
+        password = "we are connecting over localhost so this is always trusted";
         server = "mqtt://localhost:1883";
-
-        cert = "/etc/ssl/mqtt-client.crt";
-        key = "/etc/ssl/mqtt-client.key";
 
         force_disable_retain = false;
         include_device_information = true;
@@ -43,12 +49,29 @@ in with lib; {
     };
   };
 
+  # Local mosquitto instance to store-and-forward data to the global broker.
+  services.mosquitto = {
+    enable = true;
+
+    listeners = [
+      # Trust connections on localhost
+      {
+        users.zigbee2mqtt.password =
+          config.services.zigbee2mqtt.settings.mqtt.password;
+
+        # omitPasswordAuth = true;
+        address = "::1";
+        port = 1883;
+      }
+    ];
+  };
+
   services.nginx.virtualHosts = {
-    "ha.in.astrid.tech" = {
+    "ha.s02.astrid.tech" = {
       locations."/".proxyPass = "http://localhost:8123";
     };
 
-    "zigbee2mqtt.in.astrid.tech" = {
+    "zigbee2mqtt.s02.astrid.tech" = {
       locations."/".proxyPass = let z2m = config.services.zigbee2mqtt;
       in "http://localhost:${toString z2m.settings.frontend.port}";
     };
