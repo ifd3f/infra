9cc8aa80efd96074721a8ec452053b7822a1a9b6
Astrid Yu <astrid@astrid.tech>
2023-01-26 16:07:18 -0800

    tf/pc: Make separate loki server secret

--- terraform/public-cloud/monitoring.tf
+++ terraform/public-cloud/monitoring.tf
@@ -1,17 +1,19 @@
-resource "b2_application_key" "loki_storage" {
-  key_name     = "loki-storage-key"
+resource "b2_application_key" "loki_storage_v2" {
+  key_name     = "loki-storage-key-v2"
   bucket_id    = b2_bucket.logging.id
   capabilities = ["deleteFiles", "listBuckets", "listFiles", "readBucketEncryption", "readBucketReplications", "readBuckets", "readFiles", "shareFiles", "writeBucketEncryption", "writeBucketReplications", "writeFiles"]
 }
 
-resource "remote_file" "loki_secrets_env" {
-  provider = remote.durin
-
-  path        = "/var/lib/secrets/loki/secrets.env"
-  content     = <<EOF
-    S3_ACCESS=${b2_application_key.loki_storage.application_key_id}
-    S3_SECRET=${b2_application_key.loki_storage.application_key}
-  EOF
-  owner       = "loki"
-  permissions = "0600"
+resource "vault_kv_secret_v2" "loki_server_secrets" {
+  mount = "kv"
+  name  = "loki-server/environment"
+  data_json = jsonencode(
+    {
+      S3_ACCESS = b2_application_key.loki_storage_v2.application_key_id
+      S3_SECRET = b2_application_key.loki_storage_v2.application_key
+    }
+  )
+  custom_metadata {
+    max_versions = 1
+  }
 }
