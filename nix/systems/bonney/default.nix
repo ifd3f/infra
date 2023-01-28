5b6ef79509f1ed05fdb4b71a8b91411f3d45f7ff
Astrid Yu <astrid@astrid.tech>
2023-01-28 10:31:55 -0800

    nixos/bonney: Add deluge logging (jankily)

--- nix/systems/bonney/default.nix
+++ nix/systems/bonney/default.nix
@@ -1,6 +1,20 @@
 { self, nixpkgs-stable, ... }:
-self.lib.nixosSystem' {
-  nixpkgs = nixpkgs-stable;
-  system = "x86_64-linux";
-  modules = [ ./configuration.nix ];
+let
+  sys = self.lib.nixosSystem' {
+    nixpkgs = nixpkgs-stable;
+    system = "x86_64-linux";
+    modules = [ ./configuration.nix ];
+  };
+
+in sys.extendModules {
+  modules = [
+    # Monkey-patch logging onto the deluge service.
+    ({ prev, lib, ... }: {
+      systemd.services.deluged.serviceConfig.ExecStart = lib.mkForce
+        ((builtins.replaceStrings [ "\\" "\n" ] [ "" " " ]
+          prev.config.systemd.services.deluged.serviceConfig.ExecStart)
+          + " -L info");
+    })
+  ];
+  specialArgs = { prev = sys; };
 }
