6049afc361a3d919b354e5c15b72fc554c7526cc
Astrid Yu <astrid@astrid.tech>
2023-01-27 20:32:51 -0800

    bonney: Initialize configurations

--- nix/systems/bonney/configuration.nix
+++ nix/systems/bonney/configuration.nix
@@ -1,16 +1,26 @@
 # A NUC that's my media server, hooked up to the telly
-{ pkgs, lib, inputs, ... }: {
+{ pkgs, lib, inputs, ... }:
+with lib; {
   imports = [
     ./hardware-configuration.nix
+    inputs.nixos-hardware.nixosModules.common-cpu-intel
 
     inputs.self.nixosModules.server
 
     inputs.self.nixosModules.media-server
   ];
 
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  astral.tailscale.enable = mkForce false;
+
   networking = {
     hostName = "bonney";
     domain = "h.astrid.tech";
+    hostId = "49e32584";
   };
 
   time.timeZone = "US/Pacific";
@@ -20,9 +30,9 @@
     # Autologin as root because we testin here
     services.getty.autologinUser = "root";
 
+    # Route request to deluge web
     services.nginx.virtualHosts."localhost".locations."/" = {
       proxyPass = "http://localhost:80";
-      # Route request to deluge web
       extraConfig = ''
         proxy_set_header Host "deluge.s02.astrid.tech";
       '';
@@ -46,4 +56,6 @@
       ];
     };
   };
+
+  system.stateVersion = mkForce "23.05"; # Did you read the comment?
 }
