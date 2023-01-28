383c895acdfc4d28f92c40c1faf5039542888747
Astrid Yu <astrid@astrid.tech>
2023-01-28 01:43:17 -0800

    Refactor monitoring-center into multiple files

---
+++ nix/nixos-modules/roles/monitoring-center/grafana.nix
@@ -0,0 +1,71 @@
+{ pkgs, lib, config, inputs, ... }:
+with lib;
+let
+  vs = config.vault-secrets.secrets;
+  gcfg = config.services.grafana;
+in {
+  astral.backup.services.paths = [ "/var/lib/grafana" ];
+
+  # vault kv put kv/grafana-sso-oauth/env GF_AUTH_GENERIC_OAUTH_CLIENT_SECRET=@
+  vault-secrets.secrets.grafana-sso-oauth = { environmentKey = "env"; };
+
+  systemd.services.grafana = {
+    requires = [ "grafana-sso-oauth-secrets.service" ];
+    after = [ "grafana-sso-oauth-secrets.service" ];
+    serviceConfig.EnvironmentFile = "${vs.grafana-sso-oauth}/environment";
+  };
+
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        domain = "grafana.astrid.tech";
+        http_port = 2342;
+        addr = "127.0.0.1";
+        root_url = "https://grafana.astrid.tech";
+      };
+
+      "auth.generic_oauth" = {
+        name = "IFD3F Technologies";
+        icon = "signin";
+        enabled = true;
+
+        client_id = "internal-grafana";
+        client_secret = "SECRET THAT GETS OVERRIDEN";
+        scopes = "openid email profile offline_access roles";
+
+        auth_url =
+          "https://sso.astrid.tech/realms/public-users/protocol/openid-connect/auth";
+        token_url =
+          "https://sso.astrid.tech/realms/public-users/protocol/openid-connect/token";
+        api_url =
+          "https://sso.astrid.tech/realms/public-users/protocol/openid-connect/userinfo";
+        email_attribute_path = "email";
+        login_attribute_path = "username";
+        name_attribute_path = "full_name";
+        role_attribute_path =
+          "contains(roles[*], 'admin') && 'Admin' || contains(roles[*], 'editor') && 'Editor' || 'Viewer'";
+        allow_sign_up = true;
+      };
+    };
+  };
+
+  services.nginx.virtualHosts = {
+    "${gcfg.settings.server.domain}" = {
+      enableACME = true;
+      forceSSL = true;
+
+      locations."/" = {
+        proxyPass =
+          "http://127.0.0.1:${toString gcfg.settings.server.http_port}";
+        proxyWebsockets = true;
+
+        # needed to prevent 'Origin not allowed'
+        # see: https://community.grafana.com/t/after-update-to-8-3-5-origin-not-allowed-behind-proxy/60598/4
+        extraConfig = ''
+          proxy_set_header Host grafana.astrid.tech;
+        '';
+      };
+    };
+  };
+}
---
+++ nix/nixos-modules/roles/monitoring-center/xmpp-alerts.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, config, inputs, ... }:
+with lib;
+let
+  vs = config.vault-secrets.secrets;
+  gcfg = config.services.grafana;
+in {
+  astral.backup.services.paths = [ "/var/lib/grafana" "/var/lib/prometheus2" ];
+
+  # vault kv put kv/prometheus-xmpp-alerts/environment XMPP_USER_PASSWORD=@
+  vault-secrets.secrets.prometheus-xmpp-alerts = {
+    group = "prometheus-xmpp-alerts-secrets";
+  };
+
+  users.groups.prometheus-xmpp-alerts-secrets = { };
+
+  systemd.services.prometheus-xmpp-alerts.serviceConfig.EnvironmentFile =
+    "${vs.prometheus-xmpp-alerts}/environment";
+
+  services.prometheus.xmpp-alerts = {
+    enable = true;
+    settings = {
+      jid = "alertmanager@xmpp.femboy.technology";
+      to_jid = "ifd3f@xmpp.femboy.technology";
+      password_command = "echo $XMPP_USER_PASSWORD";
+      listen_address = "127.0.0.1";
+      listen_port = 9199;
+    };
+  };
+}
