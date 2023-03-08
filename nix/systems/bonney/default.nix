92cd9cb7664b693e9367c3c395c6d5e593ebcf49
Astrid Yu <astrid@astrid.tech>
2023-03-07 18:30:19 -0800

    bonney: Remove logging monkey patch

--- nix/systems/bonney/default.nix
+++ nix/systems/bonney/default.nix
@@ -1,20 +1,6 @@
 { self, nixpkgs-stable, ... }:
-let
-  sys = self.lib.nixosSystem' {
-    nixpkgs = nixpkgs-stable;
-    system = "x86_64-linux";
-    modules = [ ./configuration.nix ];
-  };
-
-in sys.extendModules {
-  modules = [
-    # Monkey-patch logging onto the deluge service.
-    ({ prev, lib, ... }: {
-      systemd.services.deluged.serviceConfig.ExecStart = lib.mkForce
-        ((builtins.replaceStrings [ "\\" "\n" ] [ "" " " ]
-          prev.config.systemd.services.deluged.serviceConfig.ExecStart)
-          + " -L info");
-    })
-  ];
-  specialArgs = { prev = sys; };
+self.lib.nixosSystem' {
+  nixpkgs = nixpkgs-stable;
+  system = "x86_64-linux";
+  modules = [ ./configuration.nix ];
 }
