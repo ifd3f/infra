d99d00d3e334728efc9429072cdda16db7a67228
Astrid Yu <astrid@astrid.tech>
2023-07-03 01:29:22 -0700

    Get rid of most of the bridges

--- nix/systems/inferno/net.nix
+++ nix/systems/inferno/net.nix
@@ -3,35 +3,25 @@ with lib; {
   networking.firewall.enable = mkForce false;
 
   networking.useDHCP = false; # DHCP not by default
-  networking.interfaces = {
-    br-user.useDHCP = true;
 
-    br-mgmt = {
-      useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.69.4";
-        prefixLength = 16;
-      }];
-    };
+  # Motherboard port, accessible for debugging purposes.
+  networking.interfaces.enp0s31f6 = {
+    useDHCP = true;
+    tempAddress = "enabled";
   };
 
-  networking.bridges = {
-    br-mb.interfaces = [ "enp0s31f6" ];
-    br-swtrunk.interfaces = [ "enp3s0" ];
-
-    br-user.interfaces = [ "sw.user" ];
-    br-mgmt.interfaces = [ "sw.mgmt" ];
+  # This is connected to vlan 69, albeit indirectly.
+  networking.interfaces.mgmtlink = {
+    virtual = true;
+    ipv4.addresses = [{
+      address = "192.168.69.10";
+      prefixLength = 24;
+    }];
+    tempAddress = "disabled";
   };
 
-  networking.vlans = {
-    "sw.mgmt" = {
-      id = 69;
-      interface = "enp3s0";
-    };
-
-    "sw.user" = {
-      id = 10;
-      interface = "enp3s0";
-    };
-  };
+  # These networking ports are directly attached to the firewall VM,
+  # via macvtap devices.
+  networking.interfaces.enp3s0.tempAddress = "disabled"; # switch trunk
+  networking.interfaces.enp4s0.tempAddress = "disabled"; # WAN
 }
