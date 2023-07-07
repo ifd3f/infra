8abc368a5cf82086710bfc89025b22875f581a5b
Astrid Yu <astrid@astrid.tech>
2023-07-07 15:01:52 -0700

    Update filesystems

--- nix/systems/gfdesk/fs.nix
+++ nix/systems/gfdesk/fs.nix
@@ -5,6 +5,16 @@
     options = [ "size=256M" ];
   };
 
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/59f761af-8370-4cc4-819e-593aee41d887";
+    fsType = "ext4";
+  };
+
+  fileSystems."/etc" = {
+    device = "ssdpool/etc";
+    fsType = "zfs";
+  };
+
   fileSystems."/nix" = {
     device = "ssdpool/nix";
     fsType = "zfs";
@@ -18,16 +28,6 @@
   fileSystems."/var" = {
     device = "ssdpool/var";
     fsType = "zfs";
-  };
-
-  fileSystems."/etc" = {
-    device = "ssdpool/etc";
-    fsType = "zfs";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/18E0-62AE";
-    fsType = "vfat";
   };
 
   swapDevices = [ ];
