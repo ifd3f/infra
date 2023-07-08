92f47b47e02bb14cb4f53fe4d9796bdcab02fe86
Astrid Yu <astrid@astrid.tech>
2023-07-07 18:00:28 -0700

    Reimage gfdesk (#246)
    * Clean up configuration.nix and make a new hardware configuration
    
    * Split up hardware-configuration.nix
    
    * Change boot device after, uh, stuff
    
    * Add /etc as a filesystem
    
    * Update filesystems
    
    * scussy
    
    * Add splash img to gfdesk
    
    * Revert "scussy"
    
    This reverts commit b70e36bc4ba84bd065b9e9d2e119382e37c299ab.

--- nix/systems/gfdesk/configuration.nix
+++ nix/systems/gfdesk/configuration.nix
@@ -1,7 +1,15 @@
 # The desk that is used by Good Friends.
-{ config, lib, pkgs, inputs, ... }:
+{ lib, inputs, modulesPath, ... }:
 with lib; {
-  imports = [ ./hardware-configuration.nix inputs.self.nixosModules.server ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+
+    inputs.self.nixosModules.server
+
+    ./boot.nix
+    ./fs.nix
+    ./net.nix
+  ];
 
   astral = {
     users.alia.enable = true;
@@ -11,52 +19,14 @@ with lib; {
     };
     monitoring-node.scrapeTransport = "tailscale";
     tailscale.enable = mkForce false;
-    net.zerotier.public = true;
   };
 
   time.timeZone = "US/Pacific";
-
-  # Explicitly don't reboot on kernel upgrade. This server takes forever to reboot, plus 
-  # it's a jet engine when it boots and it will probably wake me up at 4:00 AM
-  system.autoUpgrade.allowReboot = false;
 
   networking = {
     hostName = "gfdesk";
     domain = "h.astrid.tech";
 
     hostId = "6d1020a1"; # Required for ZFS
-    useDHCP = false;
-
-    firewall.allowedTCPPorts = [ 25565 ];
-
-    # Primary internet connection
-    interfaces.eno1.useDHCP = true;
-    interfaces.br0.useDHCP = true;
-    bridges.br0.interfaces = [ "eno1" ];
-
-    # Internal kubernetes cluster bridge
-    interfaces.brk8s.useDHCP = false;
-    interfaces.eno2.useDHCP = false;
-    interfaces.eno3.useDHCP = false;
-    interfaces.eno4.useDHCP = false;
-    bridges.brk8s.interfaces = [ "eno2" "eno3" "eno4" ];
-  };
-
-  # Use the GRUB 2 boot loader.
-  boot = {
-    loader.grub = {
-      enable = true;
-      copyKernels = true;
-      # HP G8 only supports BIOS, not UEFI
-      device = "/dev/disk/by-id/wwn-0x600508b1001c5e757c79ba52c727a91f";
-    };
-
-    initrd = {
-      availableKernelModules =
-        [ "ehci_pci" "ata_piix" "uhci_hcd" "hpsa" "usb_storage" "sd_mod" ];
-      kernelModules = [ ];
-    };
-
-    extraModulePackages = [ ];
   };
 }
