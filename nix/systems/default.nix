f895f26870d1d49162ad8991ce8801009affbaea
Astrid Yu <astrid@astrid.tech>
2023-07-10 18:41:45 -0700

    temporarily blacklist amiya for being borked

--- nix/systems/default.nix
+++ nix/systems/default.nix
@@ -1,7 +1,7 @@
 # Import every non-blacklisted folder in this directory
 { inputs, lib }:
 with lib;
-let blacklist = [ "bennett" "donkey" ];
+let blacklist = [ "amiya" "bennett" "donkey" ];
 in (mapAttrs (dir: _: (import "${./.}/${dir}" inputs)) (filterAttrs
   (name: type: type == "directory" && !builtins.elem name blacklist)
   (builtins.readDir ./.)))
