ff6a46eb839ec9509d1ad216d8cb1467ace88a6b
Astrid Yu <astrid@astrid.tech>
2023-07-12 22:33:35 -0700

    Fix pg_hba.conf for gfdesk replication

--- nix/systems/diluc/configuration.nix
+++ nix/systems/diluc/configuration.nix
@@ -90,8 +90,8 @@ with lib; {
 
     authentication = ''
       # allow gfdesk_replicator to authenticate
-      hostssl all gfdesk_replicator 0.0.0.0/0 md5
-      hostssl all gfdesk_replicator ::/0 md5
+      hostssl replication gfdesk_replicator 0.0.0.0/0 md5
+      hostssl replication gfdesk_replicator ::/0 md5
     '';
   };
 
