7993cdc8f3282ff47ac224227bd6ae660bbd71ca
Astrid Yu <astrid@astrid.tech>
2023-10-11 22:20:05 -0700

    Enable proprietary codecs

--- nix/nixos-modules/roles/pc.nix
+++ nix/nixos-modules/roles/pc.nix
@@ -192,4 +192,18 @@ in {
       libsForQt5.fcitx5-qt
     ];
   };
+
+  # Enable proprietary codecs
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 }
