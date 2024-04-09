afdb4c577645ff89f585d79e52aeb7817aaae471
Astrid Yu <astrid@astrid.tech>
2024-04-09 00:07:01 -0700

    add in the basic setup stuff

--- netconf/asmodeus.rkt
+++ netconf/asmodeus.rkt
@@ -8,6 +8,36 @@
 
 (define commands
   `((delete protocols bgp)
+    (delete firewall)
+
+    ; bunch of default crap
+    (set interfaces ethernet eth0 address "dhcp")
+    (set interfaces ethernet eth0 hw-id "52:54:00:40:30:53")
+    (set interfaces loopback lo)
+    (set protocols static route 0.0.0.0/0 next-hop 192.168.122.1)
+    (set service ntp allow-client address "0.0.0.0/0")
+    (set service ntp allow-client address "::/0")
+    (set service ntp server time1.vyos.net)
+    (set service ntp server time2.vyos.net)
+    (set service ntp server time3.vyos.net)
+    (set service ssh port "22")
+    (set system config-management commit-revisions "10000")
+    (set system conntrack modules ftp)
+    (set system conntrack modules h323)
+    (set system conntrack modules nfs)
+    (set system conntrack modules pptp)
+    (set system conntrack modules sip)
+    (set system conntrack modules sqlnet)
+    (set system conntrack modules tftp)
+    (set system console device ttyS0 speed "115200")
+    (set system host-name "asmodeus")
+    (set system login user vyos authentication public-keys chungus key "AAAAC3NzaC1lZDI1NTE5AAAAIEl4yuE1X4IqjBqt/enMyZFZKJQLxeq34BTCNqey59aZ")
+    (set system login user vyos authentication public-keys chungus type "ssh-ed25519")
+    (set system name-server "8.8.4.4")
+    (set system name-server "8.8.8.8")
+    (set system syslog global facility all level "info")
+    (set system syslog global facility local7 level "debug")
+
     ,(dn42/rpki)
     ,(dn42/bgp-setup)
     ,(dn42/bgp-group)
@@ -23,7 +53,10 @@
     ,(firewall/network-group "dn42-allowed-transit" dn42-allowed-transit-addrs)
     ,(firewall/network-group "ifd3f-dn42" ifd3f-dn42-addrs)
     ,(router-rules)
-    ,(afall (dn42-tunnels-in))))
+    ,(afall (dn42-tunnels-in))
+
+    ; address for testing
+    (set interfaces wireguard wg4242422717 address "fd00:ca7:b015:7e57::7e57/64")))
 
 (for ([s (commandtree->strings commands)])
   (displayln s))
