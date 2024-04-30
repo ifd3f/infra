4ae3cb3a11761e9fc04d49d41e178a59cf91455b
Astrid Yu <astrid@astrid.tech>
2024-04-29 21:31:38 -0700

    thatcher: waaa it no work

--- machines/thatcher/net.nix
+++ machines/thatcher/net.nix
@@ -10,14 +10,16 @@ with lib; {
       matchConfig.Type = "ether";
       networkConfig = {
         Description = "Primary network connection";
-        DHCP = "yes";
+        DHCP = "ipv4";
         IPv6AcceptRA = true;
       };
-      address = [
-        "100.64.0.45/31"
-        "2a11:f2c0:3:16::1/64"
+      addresses = [
+        { addressConfig.Address = "100.64.0.45/31"; }
+        { addressConfig.Address = "2a11:f2c0:3:16::1/64"; }
       ];
       routes = [{ routeConfig.Gateway = "100.64.0.44"; }];
     };
   };
+
+  environment.systemPackages = with pkgs; [dhcpcd];
 }
