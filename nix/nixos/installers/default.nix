40328582edd784a35ca1de7d1a1c58ccacbced3d
Astrid Yu <astrid@astrid.tech>
2025-11-08 14:37:58 -0800

    a

--- nix/nixos/installers/default.nix
+++ nix/nixos/installers/default.nix
@@ -2,6 +2,38 @@
 let
   nixpkgs = inputs.nixpkgs-stable;
 
+  # TMP(2025-11-08): zfs isn't stable on 6.17 yet
+  zfsOverlay =
+    final: prev:
+    let
+      zfsVersion = "2.4.0-rc3";
+      zfsSrc = final.fetchurl {
+        url = "https://github.com/openzfs/zfs/archive/refs/tags/zfs-${zfsVersion}.tar.gz";
+        sha256 = "";
+      };
+    in
+    {
+      zfs = prev.zfs.overrideAttrs {
+        meta.broken = false;
+        version = zfsVersion;
+        src = zfsSrc;
+      };
+
+      linuxPackagesFor =
+        args:
+        let
+          kprev = (prev.packagesFor args);
+        in
+        kprev
+        // {
+          zfs = kprev.zfs.overrideAttrs {
+            meta.broken = false;
+            version = zfsVersion;
+            src = zfsSrc;
+          };
+        };
+    };
+
   # yoinkage from https://github.com/nix-community/nixos-apple-silicon/blob/e01011ebc0aa7a0ae6444a8429e91196addd45f4/flake.nix#L61
   apple-aarch64 = nixpkgs.lib.nixosSystem {
     inherit system;
@@ -20,7 +52,10 @@ let
         # `hardware.asahi.pkgsSystem`
         nixpkgs.hostPlatform.system = "aarch64-linux";
         nixpkgs.buildPlatform.system = system;
-        nixpkgs.overlays = [ inputs.nixos-apple-silicon.overlays.default ];
+        nixpkgs.overlays = [
+          inputs.nixos-apple-silicon.overlays.default
+          zfsOverlay
+        ];
       }
     ];
   };
