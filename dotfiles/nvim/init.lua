e5d0a2212590edcaea7ceb2ed0e3397c583aab85
Astrid Yu <astrid@astrid.tech>
2025-12-21 22:37:28 -0800

    init.lua neovim

---
+++ dotfiles/nvim/init.lua
@@ -0,0 +1,32 @@
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.list = true
+
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes:1"
+vim.opt.scrolloff = 8
+vim.opt.showcmd = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+vim.opt.undofile = true
+vim.opt.clipboard = "unnamed"
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.showmode = false
+
+-- No automatic comment insertion
+vim.cmd([[autocmd FileType * set formatoptions-=ro]])
