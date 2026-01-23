cce26c59adc6998555385169f7fa663856129f07
Astrid Yu <astrid@astrid.tech>
2026-01-23 02:29:33 -0800

    install openttd
    can't believe jade got me into this game smh

--- nix/nixos/modules/program-sets/graphics/games.nix
+++ nix/nixos/modules/program-sets/graphics/games.nix
@@ -19,5 +19,15 @@
     gamescope
     lutris
     prismlauncher
+
+    (openttd-jgrpp.overrideAttrs (oldAttrs: {
+      src = fetchFromGitHub {
+        owner = "JGRennison";
+        repo = "OpenTTD-patches";
+        rev = "jgrpp-0.69.2";
+        sha256 = "sha256-D9Oh05Isf7Atsih6tZBA8xS04aCuE8VS5Ghf0FvrU5A=";
+      };
+      version = "0.69.2";
+    }))
   ];
 }
