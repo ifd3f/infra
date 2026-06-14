f71e711cbf618b23d5cd94115263153747c8eca4
Astrid Yu <astrid@astrid.tech>
2026-06-14 01:50:29 -0700

    rm bambu studio bc builds cause OOM

--- nix/nixos/modules/program-sets/graphics/cad.nix
+++ nix/nixos/modules/program-sets/graphics/cad.nix
@@ -15,7 +15,7 @@ in
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       #kicad
-      bambu-studio
+      # bambu-studio causing ooms while compiling???
       openscad-unstable
       freecad
     ];
