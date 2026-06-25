03f587cc34f8ac64fd64da144ee3fb813726a2fb
Astrid Yu <astrid@astrid.tech>
2026-06-24 20:56:42 -0700

    chungus: switch to limine

--- nix/nixos/machines/pc/chungus/boot.nix
+++ nix/nixos/machines/pc/chungus/boot.nix
@@ -1,7 +1,31 @@
 { pkgs, inputs, ... }:
+let
+  wallpaper =
+    with pkgs;
+    runCommand "wallpaper.png" { buildInputs = [ imagemagick ]; } ''
+      magick -background "#000000" "${inputs.self}/nix/nixowos.svg" nixowos.png
+      mv nixowos.png $out
+    '';
+in
 {
+  _class = "nixos";
+
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.loader.grub.enable = false;
-  boot.loader.refind.enable = true;
+  boot.loader.limine = {
+    enable = true;
+    efiSupport = true;
+    biosSupport = false;
+
+    # While the docs say that "it allows gaining root access by passing init=/bin/sh as a kernel parameter,"
+    # I have encrypted my drive, so it doesn't actually matter :)
+    enableEditor = true;
+
+    style = {
+      interface.branding = "meowmeowmeowmeowmeowmmemwmwemewmmweoew";
+      wallpapers = [ wallpaper ];
+      wallpaperStyle = "tiled";
+    };
+  };
 }
