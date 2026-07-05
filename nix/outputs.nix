a649869ded2049357373b74137b83be282aae61e
Astrid Yu <astrid@astrid.tech>
2026-07-04 20:19:01 -0700

    break flake outputs into their own module

---
+++ nix/outputs.nix
@@ -0,0 +1,72 @@
+{
+  self,
+  inputs,
+  config,
+  ...
+}:
+{
+  imports = [
+    inputs.home-manager.flakeModules.home-manager
+
+    ./rescue
+    ./nixos/machines
+  ];
+
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+    "x86_64-darwin"
+    "aarch64-darwin"
+  ];
+
+  astral = {
+    machines = {
+      nixosSystem = inputs.nixpkgs-stable.lib.nixosSystem;
+      nixos-hardware = inputs.nixos-hardware;
+      overlay = self.overlays.default;
+    };
+  };
+
+  flake = {
+    overlays.default = final: prev: {
+      inherit (inputs.nixpkgs-unstable.legacyPackages.${prev.system})
+        # TODO: trilium is out of date on stable, remove when it's updated
+        trilium
+        trilium-server
+        ;
+    };
+    nixosModules = rec {
+      astral = { pkgs, lib, ... }: {
+        imports = [ ./nixos/modules ];
+        astral.inputs.sshKeyDatabase = import ../ssh_keys;
+        services.armqr.package = lib.mkDefault inputs.armqr.packages.${pkgs.system}.default;
+      };
+      default = astral;
+    };
+    homeConfigurations = {
+      astrid = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs-stable { system = "x86_64-linux"; };
+        modules = [ ./home-manager/basic.nix ];
+      };
+    };
+  };
+
+  perSystem =
+    {
+      config,
+      pkgs,
+      system,
+      ...
+    }:
+    {
+      devShells = import ./shells.nix {
+        inherit self;
+        pkgs = import inputs.nixpkgs-stable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
+
+      legacyPackages.helpers = pkgs.callPackage ./helpers.nix { };
+    };
+}
