c525aace562eb7dfb7fc6b3f8888ba416de2fe79
Astrid Yu <astrid@astrid.tech>
2026-08-03 01:11:58 -0700

    shrink the thing

--- nixos/machines/pc/twinkpaw/default.nix
+++ nixos/machines/pc/twinkpaw/default.nix
@@ -23,23 +23,18 @@ with lib;
     hostId = "76d4a2bc";
   };
 
-  astral.roles.pc.enable = true;
+  astral = {
+    sshd.enable = true;
+    zfs-utils.enable = true;
+  };
+
+  programs.sway.enable = true;
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.enableRedistributableFirmware = lib.mkDefault true;
 
   time.timeZone = "US/Pacific";
-
-  services.xserver.dpi = 209;
-
-  environment.systemPackages = with pkgs; [
-    # Screen has a problem of blanking randomly. I don't know why it does this,
-    # but either way, this is a script that does the necessary unfucking procedure.
-    (writeShellScriptBin "ufsc" ''
-      xrandr --output eDP-1 --off && xrandr --output eDP-1 --auto
-    '')
-  ];
 
   system.stateVersion = "25.05";
 }
