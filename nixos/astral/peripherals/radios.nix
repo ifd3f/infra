a8194026047d4451b34640e762025eba083efc4b
Astrid Yu <astrid@astrid.tech>
2026-08-15 15:48:57 -0700

    disable sdrplay because it's not compiling and i dont need it

--- nixos/astral/peripherals/radios.nix
+++ nixos/astral/peripherals/radios.nix
@@ -10,6 +10,8 @@ in
   config = lib.mkIf cfg.enable {
     hardware.hackrf.enable = true;
     hardware.rtl-sdr.enable = true;
-    services.sdrplayApi.enable = true;
+
+    # TODO: disabled as of 2026-08-15 for not working
+    # services.sdrplayApi.enable = true;
   };
 }
