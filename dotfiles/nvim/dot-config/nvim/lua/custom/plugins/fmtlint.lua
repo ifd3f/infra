29a24cacb97c8ebad051c7b929330e316afbc0a3
Astrid Yu <astrid@astrid.tech>
2026-08-30 14:36:28 -0700

    nvim: disable markdown linter

--- dotfiles/nvim/dot-config/nvim/lua/custom/plugins/fmtlint.lua
+++ dotfiles/nvim/dot-config/nvim/lua/custom/plugins/fmtlint.lua
@@ -41,41 +41,9 @@ function M.setup()
     vim.pack.add { 'https://github.com/mfussenegger/nvim-lint' }
 
     local lint = require 'lint'
-    lint.linters_by_ft = {
-      markdown = { 'markdownlint' }, -- Make sure to install `markdownlint` via mason / npm
-    }
 
-    -- To allow other plugins to add linters to require('lint').linters_by_ft,
-    -- instead set linters_by_ft like this:
-    -- lint.linters_by_ft = lint.linters_by_ft or {}
-    -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-    --
-    -- However, note that this will enable a set of default linters,
-    -- which will cause errors unless these tools are available:
-    -- {
-    --   clojure = { "clj-kondo" },
-    --   dockerfile = { "hadolint" },
-    --   inko = { "inko" },
-    --   janet = { "janet" },
-    --   json = { "jsonlint" },
-    --   markdown = { "vale" },
-    --   rst = { "vale" },
-    --   ruby = { "ruby" },
-    --   terraform = { "tflint" },
-    --   text = { "vale" }
-    -- }
-    --
-    -- You can disable the default linters by setting their filetypes to nil:
-    -- lint.linters_by_ft['clojure'] = nil
-    -- lint.linters_by_ft['dockerfile'] = nil
-    -- lint.linters_by_ft['inko'] = nil
-    -- lint.linters_by_ft['janet'] = nil
-    -- lint.linters_by_ft['json'] = nil
-    -- lint.linters_by_ft['markdown'] = nil
-    -- lint.linters_by_ft['rst'] = nil
-    -- lint.linters_by_ft['ruby'] = nil
-    -- lint.linters_by_ft['terraform'] = nil
-    -- lint.linters_by_ft['text'] = nil
+    -- disable that shit
+    lint.linters_by_ft['markdown'] = nil
 
     -- Create autocommand which carries out the actual linting
     -- on the specified events.
